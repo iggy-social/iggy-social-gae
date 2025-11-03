6fe24f474c2985765ce308f8384f07d8d9e116d8
Tempe Techie <95053628+tempe-techie@users.noreply.github.com>
2025-11-03 20:13:46 +0100

    highest priced NFTs endpoint and page

--- components/nft/list/NftListDropdown.vue
+++ components/nft/list/NftListDropdown.vue
@@ -12,9 +12,9 @@
       <li><NuxtLink class="dropdown-item" to="/nft/featured"><i class="bi bi-patch-check me-2"></i>Featured NFTs</NuxtLink></li>
       <!--
       <li><NuxtLink class="dropdown-item" to="/nft/most-traded"><i class="bi bi-arrow-left-right me-2"></i>Most Traded NFTs</NuxtLink></li>
-      
-      <li><NuxtLink class="dropdown-item" to="/nft/highest-price"><i class="bi bi-currency-bitcoin me-2"></i>Top-Priced NFTs</NuxtLink></li>
-      
+      -->
+      <li><NuxtLink class="dropdown-item" to="/nft/highest-priced"><i class="bi bi-currency-bitcoin me-2"></i>Top-Priced NFTs</NuxtLink></li>
+      <!--
       <li><NuxtLink class="dropdown-item" to="/nft/most-holders"><i class="bi bi-gem me-2"></i>Most Holders NFTs</NuxtLink></li>
       -->
     </ul>
